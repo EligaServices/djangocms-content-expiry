b59c059209a1c803749669e051f84bf6480017d7
bernard <bvdv9075@gmail.com>
2021-08-16 15:07:10 +0100

    Changes requested: Reverted test back to test_basic.py and replaced single migration file

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+from cms.test_utils.testcases import CMSTestCase
+
+
+class BasicTest(CMSTestCase):
+    """
+    Basic test to ensure test suit runs
+    """
+
+    def test_fail(self):
+        self.assertTrue(True)
