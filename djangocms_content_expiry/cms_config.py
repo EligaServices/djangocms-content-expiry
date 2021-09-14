2ba05e2e6455cc153808687bf965cb3a3284682f
Andrew Aikman <andrew.aikman@eliga.services>
2021-09-14 16:39:49 +0100

    Remove the default add button control for the content expiry link int he moderation config

--- djangocms_content_expiry/cms_config.py
+++ djangocms_content_expiry/cms_config.py
@@ -8,7 +8,7 @@ from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
 
 
-def _get_moderation_content_expiry_link(obj, request):
+def _get_moderation_content_expiry_link(obj):
     """
     Return a user friendly button for viewing content expiry in djangocms-moderation
     :param obj: TODO
@@ -26,12 +26,6 @@ def _get_moderation_content_expiry_link(obj, request):
         return render_to_string(
             "djangocms_content_expiry/calendar_icon.html", {"url": view_endpoint, "field_id": f"contentexpiry_{obj.pk}"}
         )
-    # Otherwise we need to be able to create a new content expiry record
-    add_endpoint = reverse('admin:djangocms_content_expiry_contentexpiry_add')
-    compiled_add_endpoint = f"{add_endpoint}?version={version.pk}&created_by={request.user.pk}"
-    return render_to_string(
-        "djangocms_content_expiry/calendar_plus_icon.html", {"url": compiled_add_endpoint}
-    )
 
 
 def get_expiry_date(obj):
