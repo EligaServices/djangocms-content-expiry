b4efd5565f245f3b1680d5cc8012faa465bf2f86
Andrew Aikman <andrew.aikman@eliga.services>
2021-09-22 16:11:03 +0100

    WIP, merge error, saving progress

--- djangocms_content_expiry/models.py
+++ djangocms_content_expiry/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
 
 from djangocms_versioning.models import Version
@@ -22,3 +23,31 @@ class ContentExpiry(models.Model):
     class Meta:
         verbose_name = _("Content Expiry")
         verbose_name_plural = _("Content Expiry")
+
+
+from djangocms_versioning.versionables import _cms_extension
+
+def limit_pub_date_choices():
+    content_type_choices = []
+    for content_type in _cms_extension().versionables_by_content:
+        value = ContentType.objects.get_for_model(content_type)
+        content_type_choices.append((value.pk, value))
+        return content_type_choices
+
+
+class ContentExpiryContentTypeConfiguration(models.Model):
+    duration = models.IntegerField(help_text=_("duration in days"))
+    content_type = models.OneToOneField(
+        ContentType,
+        primary_key=True,
+        limit_choices_to=limit_pub_date_choices,
+        on_delete=models.CASCADE,
+        verbose_name=_('content type')
+    )
+
+    class Meta:
+        verbose_name = _("Content Type Configuration")
+        verbose_name_plural = _("Content Type Configuration")
+
+
+
